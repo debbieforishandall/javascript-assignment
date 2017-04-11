808f1ea68da1913f5b7d268c1fd2aa6659a0679b
debbieforishandall <temilola.oloyede@bison.howard.edu>
2017-04-11 17:02:14 -0400

    Added image.css, image.js and image.html
    Added skeletal image.css, image.js and image.html code

---
+++ image.js
@@ -0,0 +1,19 @@
+//Javascript assignment to add images from user inputed urls to a planning area
+//This is the .js file for the behaviour of this project
+
+//Checks if a url is valid,if so, adds it to the planning area
+function addImage(){
+	
+}
+
+//Checks if image with given url is on the planning area, 
+//if so deletes it from the planning area
+function deleteImage(){
+	
+}
+
+window.onload=function(){
+	$("add").onclick = addImage;
+	$("delete").onclick = deleteImage;
+}
+
