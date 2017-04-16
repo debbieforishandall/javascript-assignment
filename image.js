6c1761736b38b0fe0f18608fd7218ff92b4c507c
debbieforishandall <temilola.oloyede@bison.howard.edu>
2017-04-16 19:43:21 -0400

    Added onclick to images in the planning area

--- image.js
+++ image.js
@@ -8,21 +8,31 @@ function addImage(url){
 	console.log("Valid Url: " + url.match(pattern));
 	//Add the image
 	var elem = document.createElement("img");
+	//check image loaded
+	$("elem").on('load', function() {
+	  console.log("image is loaded in addImage(url)");
+	});
+	//Add image source
 	elem.src = url;
-	elem.setAttribute("alt", "planning");
-	elem.setAttribute("id", url);
-	$("planning-area").appendChild(elem);
+	//Get the height and Width of the image
 	var height = parseInt(elem.height);
     var width = parseInt(elem.width);
 	console.log("height: " + height);
 	console.log("widht: " + width);
+	//Set height to 100 if greater than 100
 	if(height > 100){
 		console.log("greater");
 		elem.setAttribute("height", "100");
 	}
+	//Set width to 100 if greater than 100
 	if(width > 100){
 		elem.setAttribute("width", "100");
 	}
+	//Set alt text and set image id as url
+	elem.setAttribute("alt", "planning");
+	elem.setAttribute("id", url);
+	//append the image to the planning area
+	$("planning-area").appendChild(elem);
 	
 }
 
@@ -41,7 +51,12 @@ window.onload=function(){
 		addImage($("input-url").value);
 	});
 	$("delete").onclick = deleteImage;
-	//add the onClick method to all the children of planning-area
 	
+	//add the onClick method to all the children of planning-area
+	var noImages = document.getElementById("planning-area").childNodes.length;
+	var images = document.getElementById("planning-area").childNodes;
+	for(var i = 0; i < noImages; i++){
+		images[i].onclick = imageClick;
+	}
 }
 
