fbe3b01435558b6851a89fe9485e90980f51d5f4
debbieforishandall <temilola.oloyede@bison.howard.edu>
2017-04-19 08:01:44 -0400

    Fixed clear error tolook for keyup event of input url instead of keypress

--- image.js
+++ image.js
@@ -215,7 +215,7 @@ window.onload=function () {
 		deleteImage($("input-url").value);
 	});
 	//On input into the input-url element, clear errors
-	$("input-url").onkeypress = clearError;
+	$("input-url").onkeyup = clearError;
 	
 	//add the onClick method to all the children of planning-area
 	//var noImages = document.getElementById("planning-area").childNodes.length;
