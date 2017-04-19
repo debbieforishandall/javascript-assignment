bdc0db9c6cc7e1e1b2062db21d738ca316023909
debbieforishandall <temilola.oloyede@bison.howard.edu>
2017-04-19 07:02:46 -0400

    Fixed jslint warnings

--- image.js
+++ image.js
@@ -1,11 +1,76 @@
 //Javascript assignment to add images from user inputed urls to a planning area
 //This is the .js file for the behaviour of this project
 
+ /*jslint devel: true */
+ /*jshint smarttabs:true */
+ /*jslint white: true */
+ /*global $ */
+ /*jslint browser: true*/
+ /*global window*/
+ /*jslint this: true */
+ 
+//Clears the error paragraph
+function clearError() {
+	"use strict";
+	//Set error text to empty
+	$("error").innerHTML = "";
+}
+
+//Handles clicks on image
+function imageClick() {
+	"use strict";
+	console.log("In imageClick(), id is: " + this.id);
+	$("input-url").value = this.id;
+}
+
+//Checks if url is valid
+function isValidUrl(url) {
+	"use strict";
+	//TODO: fix this
+	var pattern = /^(http\:\/\/|https\:\/\/)?([a-z0-9][a-z0-9\-]*\.)+[a-z0-9][a-z0-9\-]*$/i;
+	console.log("Valid Url: " + url.match(pattern));
+	return true;
+}
+
+//Zooms focused picture
+function zoomPicture() {
+	"use strict";
+	console.log("In zoomPicture");
+	//Get original width
+	var origHeight = this.naturalHeight;
+	var origWidth = this.naturalWidth;
+	console.log("In zoomPicture, origHeight: " + origHeight);
+	if(origHeight > 500 && origWidth > 500){
+		this.height = 500;
+		this.width = 500;
+	} else{
+		this.height = origHeight;
+		this.width = origWidth;
+	}	
+}
+
+//zooms out of unfocused picture
+function zoomOut() {
+	"use strict";
+	if(this.naturalHeight > 100){
+		this.height = 100;
+	} else {
+		this.height = this.naturalHeight;
+	}
+	if(this.naturalwidth > 100){
+		this.width = 100;
+	} else {
+		this.width = this.naturalWidth;
+	}
+	
+}
+
 //Checks if a url is valid,if so, adds it to the planning area
-function addImage(url){
+function addImage(url) {
+	"use strict";
 	clearError();
 	//check if url is valid
-	if(isValidUrl(url) ){
+	if(isValidUrl(url)) {
 		//Add the image
 		var elem = document.createElement("img");
 		var height = 0;
@@ -19,9 +84,9 @@ function addImage(url){
 			console.log("height: " + height);
 			console.log("widht: " + width);
 			//Add mouse eneter and mouse leave functions
-			elem.addEventListener('mouseover', zoomPicture);
-			elem.addEventListener('mouseout', zoomOut);			
-		}
+			elem.addEventListener("mouseover", zoomPicture);
+			elem.addEventListener("mouseout", zoomOut);			
+		};
 		//Add image source
 		elem.src = url;
 		
@@ -29,15 +94,15 @@ function addImage(url){
 		console.log("outside width: " + width);	
 		
 		//Set height to 100 if greater than 100
-		if(height > 100){
+		if(height > 100) {
 			console.log("greater");
 			elem.setAttribute("height", "100");
-		} else{
+		} else {
 			console.log("less");
 			elem.setAttribute("height", height);
 		}
 		//Set width to 100 if greater than 100
-		if(width > 100){
+		if(width > 100) {
 			elem.setAttribute("width", "100");
 		} else {
 			elem.setAttribute("width", width);
@@ -45,7 +110,7 @@ function addImage(url){
 		//Set alt text and set image id as url
 		elem.setAttribute("alt", "planning image");
 		elem.setAttribute("id", url);
-		elem.addEventListener('click', imageClick);
+		elem.addEventListener("click", imageClick);
 		
 		//append the image to the planning area
 		$("planning-area").appendChild(elem);
@@ -58,16 +123,17 @@ function addImage(url){
 
 //Checks if image with given url is on the planning area, 
 //if so deletes it from the planning area
-function deleteImage(url){
+function deleteImage(url) {
+	"use strict";
 	clearError();
 	//check if url is valid
-	if(isValidUrl(url)){
+	if(isValidUrl(url)) {
 		//check  if url is on planning area
-		if($(url)){
-			//TODO: delete the object
-			var elem = $(url).remove();
+		if($(url)) {
+			//delete the object
+			$(url).remove();
 		}
-		else{
+		else {
 			//add an error to the error text
 			$("error").innerHTML = "Error image with specified url is not in planning area";
 		}
@@ -79,65 +145,17 @@ function deleteImage(url){
 	
 }
 
-//Clears the error paragraph
-function clearError(){
-	//Set error text to empty
-	$("error").innerHTML = "";
-}
-
-//Handles clicks on image
-function imageClick(){
-	console.log("In imageClick(), id is: " + this.id);
-	$("input-url").value = this.id;
-}
-
-function isValidUrl(url){
-	//TODO: fix this
-	var pattern = /^(http\:\/\/|https\:\/\/)?([a-z0-9][a-z0-9\-]*\.)+[a-z0-9][a-z0-9\-]*$/i;
-	console.log("Valid Url: " + url.match(pattern));
-	return true;
-}
-
-//Zooms focused picture
-function zoomPicture(){
-	console.log("In zoomPicture");
-	//Get original width
-	var origHeight = this.naturalHeight;
-	var origWidth = this.naturalWidth;
-	console.log("In zoomPicture, origHeight: " + origHeight);
-	if(origHeight > 500 &&  origWidth > 500){
-		this.height = 500;
-		this.width = 500;
-	} else{
-		this.height = origHeight;
-		this.width = origWidth;
-	}	
-}
-
-function zoomOut(){
-	if(this.naturalHeight > 100){
-		this.height = 100;
-	} else {
-		this.height = this.naturalHeight;
-	}
-	if(this.naturalwidth < 100){
-		this.width = 100;
-	} else {
-		this.width = this.naturalWidth;
-	}
-	
-}
-
-window.onload=function(){
-	$("add").addEventListener('click', function(){
+//window load function
+window.onload=function () {
+	"use strict";
+	$("add").addEventListener("click", function () {
 		addImage($("input-url").value);
 	});
-	$("delete").addEventListener('click', function(){
+	$("delete").addEventListener("click", function () {
 		deleteImage($("input-url").value);
 	});
 	//On input into the input-url element, clear errors
 	$("input-url").onchange = clearError;
-	
 	
 	//add the onClick method to all the children of planning-area
 	//var noImages = document.getElementById("planning-area").childNodes.length;
@@ -145,5 +163,6 @@ window.onload=function(){
 	//for(var i = 0; i < noImages; i++){
 	//	images[i].onclick = imageClick;
 	//}
-}
+	
+};
 
